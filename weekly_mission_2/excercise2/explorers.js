062b46f332ea2e539a5e9c6bdde561ccbda30714
Alejandro Lima <73153439+Drojann@users.noreply.github.com>
2022-05-03 00:26:48 -0500

    solucion ejercicio 2

---
+++ weekly_mission_2/excercise2/explorers.js
@@ -0,0 +1,76 @@
+const { arrowFunctionExpression } = require("@babel/types")
+
+const explorers = [
+    {
+      name: "Explorer 1",
+      exercises_completed: 10,
+      rate: 99,
+      city: "CDMX",
+      stack: [
+        "js",
+        "c#"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: true,
+          exercisesFinished: true
+        }
+      }
+    },
+    {
+      name: "Explorer 2",
+      exercises_completed: 9,
+      city: "Veracruz",
+      rate: 50,
+      stack: [
+        "js"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: false,
+          exercisesFinished: false
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    },
+    {
+      name: "Explorer 3",
+      exercises_completed: 3,
+      city: "Sonora",
+      rate: 100,
+      stack: [
+        "elixir"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    }
+   ]
+
+console.log("2.1")
+explorers.forEach(explorer => console.log(explorer.name))
+
+console.log("2.2")
+explorers.forEach(item => console.log(item.stack))
+
+console.log("2.3")
+const new_stacks = explorers.map(newList => newList.stack)
+console.log(new_stacks)
+
+console.log("2.4")
+const explorer_js = explorers.filter(listJS => listJS.stack.includes("js"))
+console.log(explorer_js)
